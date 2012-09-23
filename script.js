3b9a5e4c767eff1796a3caf873784e272d0d3093
Roberto <topikito@gmail.com>
2012-09-23 11:51:27 +0200

    [A] First commit

---
+++ script.js
@@ -0,0 +1,37 @@
+jQuery(document).ready(function()
+{
+
+	var loader 					= jQuery('#loader')
+	,	iframe 					= jQuery('#toshiframe')
+	,	settings 				= jQuery('#settings')
+	,	saveSettingsButton 		= jQuery('#saveSettings')
+	,	transmissionUrlInput 	= jQuery('#transmissionUrl')
+	,	checkStorage = function()
+	{
+		loader.hide();
+		settings.hide();
+		iframe.hide();
+		transmissionUrl = '';
+		if (localStorage.transmissionUrl)
+		{
+			transmissionUrl = localStorage.transmissionUrl;
+			iframe.attr('src',transmissionUrl).show();
+		}
+		else
+		{
+			settings.show();
+		}
+	}
+	,	saveSettings = function(newData) 
+	{
+		if (newData.transmissionUrl)
+		{
+			localStorage.transmissionUrl = newData.transmissionUrl;
+		}
+		checkStorage();
+	};
+
+	checkStorage();
+	saveSettingsButton.bind('click', function() { saveSettings({ "transmissionUrl" : transmissionUrlInput.val() }); });
+
+});
